8753b7db1974bc0a1a64903f932f0d4b642faf04
Just <grivten@yandex.ru>
2024-06-22 02:27:25 +0300

    updated to 1.21 (no way)

--- src/main/java/net/just_s/sds/mixin/DebugStickMixin.java
+++ src/main/java/net/just_s/sds/mixin/DebugStickMixin.java
@@ -3,16 +3,16 @@ package net.just_s.sds.mixin;
 import net.just_s.sds.Config;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.component.DataComponentTypes;
+import net.minecraft.component.type.DebugStickStateComponent;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.DebugStickItem;
 import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NbtCompound;
+import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.Property;
 import net.minecraft.text.Text;
-import net.minecraft.util.Util;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.registry.Registries;
 import net.minecraft.world.WorldAccess;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
@@ -36,13 +36,19 @@ public class DebugStickMixin {
         return null;
     }
 
+    @Shadow
+    private static <T> T cycle(Iterable<T> values, @Nullable T value, boolean inverse) {
+        return null;
+    }
+
     @Inject(at = @At("HEAD"), method = "use", cancellable = true)
     private void onUSE(PlayerEntity player, BlockState state, WorldAccess world, BlockPos pos, boolean update, ItemStack stack, CallbackInfoReturnable<Boolean> cir) {
         // if the player already does have the rights to use Debug Stick, the mod should not interfere
         if (player.isCreativeLevelTwoOp()) {return;}
 
         Block block = state.getBlock();
-        StateManager<Block, BlockState> stateManager = block.getStateManager();
+        RegistryEntry<Block> registryEntry = state.getRegistryEntry();
+        StateManager<Block, BlockState> stateManager = (registryEntry.value()).getStateManager();
         Collection<Property<?>> collection = stateManager.getProperties();
 
         // check if block is modifiable by the config
@@ -52,22 +58,23 @@ public class DebugStickMixin {
             return;
         }
 
-        // https://minecraft.fandom.com/wiki/Debug_Stick#Item_data
+        // https://minecraft.wiki/w/Debug_Stick
         // to remember the data of which property for which block is chosen,
-        // Minecraft Devs decided to use NBT data for Debug Stick.
-        // Who am I to disagree?
-        NbtCompound nbtCompound = stack.getOrCreateSubNbt("DebugProperty");
+        // Minecraft Devs decided to use Component for Debug Stick.
+        // Who am I to disagree? (btw thx to @MrBretze for example code)
+        DebugStickStateComponent stateComponent = stack.get(DataComponentTypes.DEBUG_STICK_STATE);
 
-        String blockName = Registries.BLOCK.getId(block).toString();
-        String propertyName = nbtCompound.getString(blockName);
+        if (stateComponent == null) {
+            return;
+        }
 
-        Property<?> property = stateManager.getProperty(propertyName);
+        Property<?> property = stateComponent.properties().get(registryEntry);
 
         if (player.isSneaking()) {
             // select next property
             property = getNextProperty(collection, property, block);
             // save chosen property in the NBT data of Debug Stick
-            nbtCompound.putString(blockName, property.getName());
+            stack.set(DataComponentTypes.DEBUG_STICK_STATE, stateComponent.with(registryEntry, property));
 
             // send the player a message of successful selecting
             sendMessage(player, Text.of(
@@ -105,11 +112,11 @@ public class DebugStickMixin {
      * Choose next property that is appropriate for the configuration file
      * */
     private Property<?> getNextProperty(Collection<Property<?>> collection, @Nullable Property<?> property, @Nullable Block block) {
-        int len = collection.size();
+        int i = 0;
         do { // simply scrolling through the list of properties until suitable is found
-            property = Util.next(collection, property);
-            len--;
-        } while (len > 0 && !isPropertyModifiable(property, block));
+            property = cycle(collection, property, false);
+            i++;
+        } while (i < collection.size() && !isPropertyModifiable(property, block));
         return property;
     }
 
