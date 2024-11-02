70a5e706cf9b32f1357ad2a1734dde21471e85d4
Just <grivten@yandex.ru>
2024-11-02 15:31:46 +0300

    version update

---
+++ src/main/java/net/just_s/sds/datagen/SDSDataGenerator.java
@@ -0,0 +1,12 @@
+package net.just_s.sds.datagen;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SDSDataGenerator implements DataGeneratorEntrypoint {
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+        FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
+        pack.addProvider(SDSRecipeGenerator::new);
+    }
+}
---
+++ src/main/java/net/just_s/sds/datagen/SDSRecipeGenerator.java
@@ -0,0 +1,44 @@
+package net.just_s.sds.datagen;
+
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
+import net.just_s.sds.SDSMod;
+import net.minecraft.data.server.recipe.RecipeExporter;
+import net.minecraft.data.server.recipe.RecipeGenerator;
+import net.minecraft.item.Items;
+import net.minecraft.recipe.book.RecipeCategory;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.RegistryWrapper;
+import net.minecraft.util.Identifier;
+
+import java.util.concurrent.CompletableFuture;
+
+public class SDSRecipeGenerator extends FabricRecipeProvider {
+    public SDSRecipeGenerator(FabricDataOutput output, CompletableFuture<RegistryWrapper.WrapperLookup> registriesFuture) {
+        super(output, registriesFuture);
+    }
+
+    @Override
+    protected RecipeGenerator getRecipeGenerator(RegistryWrapper.WrapperLookup wrapperLookup, RecipeExporter recipeExporter) {
+        return new RecipeGenerator(wrapperLookup, recipeExporter) {
+            @Override
+            public void generate() {
+                createShapeless(RecipeCategory.TOOLS, Items.DEBUG_STICK)
+                        .input(Items.STICK).criterion(hasItem(Items.STICK), conditionsFromItem(Items.STICK))
+                        .input(Items.CHORUS_FRUIT).criterion(hasItem(Items.CHORUS_FRUIT), conditionsFromItem(Items.CHORUS_FRUIT))
+                        .offerTo(recipeExporter, RegistryKey.of(
+                                RegistryKeys.RECIPE,
+                                Identifier.of("sds", getRecipeName(Items.DEBUG_STICK))
+                        ));
+            }
+        };
+    }
+
+    @Override
+    public String getName() {
+        return "";
+    }
+
+
+}
